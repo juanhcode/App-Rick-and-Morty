a19a00e2dd3460a2cd4f0a3e7e6a9816f1f5acca
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-18 18:00:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rickproject',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
